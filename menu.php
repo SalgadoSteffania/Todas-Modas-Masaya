abfc87a6986d50528c072282bf62160b6a6355cc
Margie Fuertes <mareff0712@gmail.com>
2025-11-03 13:09:23 -0600

    Se agrego la opcion de departamentos en el menu lateral

--- menu.php
+++ menu.php
@@ -65,8 +65,17 @@ if (!isset($_SESSION['correo'])) {
               <img src="img/inicio.svg" class="icon" alt=""> <span class="label">Inicio</span>
             </li>
 
-            <li class="item nivel1" onclick="mostrarContenido('Clientes')">
-              <img src="img/cliente.svg" class="icon" alt=""> <span class="label">Clientes</span>
+             <li class="submenu nivel1">
+              <div class="submenu-header">
+                <div class="left">
+                  <img src="img/cliente.svg" class="icon" alt=""><span class="label">Clientes</span>
+                </div>
+                <img src="img/mas.svg" class="toggle-icon" alt="toggle">
+              </div>
+              <ul class="submenu-list">
+                <li class="item" onclick="mostrarContenido('Clientes')">Registrar Cliente</li>
+                <li class="item" onclick="mostrarContenido('Departamentos')">Departamento</li>
+              </ul>
             </li>
 
             <!-- Empleado -->
